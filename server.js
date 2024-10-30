647bbde9bfda31dfe36d20c2c110c459bd77542b
oluwatobi ayoola jolaosho <fury25423@gmail.com>
2024-10-30 15:13:27 +0100

    fixing session issue

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const corsOption = require("./config/corsOption");
 const { errorHandler } = require("./middleware/errormiddleware");
 const connectDB = require("./config/db");
 const logger = require("./utils/logger");
-const chalk = require("chalk")
+const chalk = require("chalk");
+const session = require("express-session");
 
 const app = express();
 
@@ -15,9 +16,9 @@ const port = process.env.PORT || 4040;
 // Logger
 app.use(morgan("tiny", { stream: logger.stream }));
 app.use(
-    morgan(
-        ":date[iso] - :method :url :status :res[content-length] - :response-time ms"
-    )
+  morgan(
+    ":date[iso] - :method :url :status :res[content-length] - :response-time ms"
+  )
 );
 // app.use(requestIp.mw());
 
@@ -31,23 +32,31 @@ app.use(express.urlencoded({ extended: false }));
 //authorizations endpoint
 app.set("trust proxy", true);
 app.use((req, res, next) => {
-    res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-    res.append("Access-Control-Allow-Headers", "Content-Type");
-    // Add the 'Authorization' header to the list of exposed headers
-    res.append("Access-Control-Expose-Headers", "Authorization");
-    next();
+  res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.append("Access-Control-Allow-Headers", "Content-Type");
+  // Add the 'Authorization' header to the list of exposed headers
+  res.append("Access-Control-Expose-Headers", "Authorization");
+  next();
 });
-app.use(session({ secret: process.env.sessionID, resave: false, saveUninitialized: true }));
+app.use(
+  session({
+    secret: process.env.sessionID,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 app.get("/", (req, res) => {
-    res.send("welcome to the default route, if you get this message then it means you have probably set it u, doesnt mean you wont further experience challenge");
+  res.send(
+    "welcome to the default route, if you get this message then it means you have probably set it u, doesnt mean you wont further experience challenge"
+  );
 });
 app.use("/api/v1/waitingist", require("./Routes/waitinglist"));
 // app.use("app/v1/authenticate", require("./Routes/user"))
 app.use(errorHandler);
 
 app.listen(port, () => {
-    console.log(chalk.blue.underline(`Server running on port ${port}`));
-    logger.info(`Server running on port ${port}`);
+  console.log(chalk.blue.underline(`Server running on port ${port}`));
+  logger.info(`Server running on port ${port}`);
 });
